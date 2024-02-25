0ff2a8d589edcf12d20cd29934f1ba7fd7be617c
Alena <alyonatihomirova9@gmail.com>
2024-02-25 12:51:24 +0300

    Update

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// JavaScript
+const mobileNav = document.getElementById("mobile-nav")
+
+document.addEventListener("click", function(e) {
+    if (e.target.id === "hamburger") {
+        showMenu()
+    }
+})
+
+function showMenu() {
+    mobileNav.innerHTML = `
+    <div class="mobile-menu-container" id="mobile-menu-container">
+        <ul>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#about-us">About us</a></li>
+            <li><a href="#feedback">About you</a></li>
+            <li><a href="#contact">Contact</a></li>
+        <ul>
+    </div>`
+    document.getElementById("mobile-menu-container").style.display = "flex"
+    document.getElementById("mobile-menu-container").style.flexDirection = "column"
+}
