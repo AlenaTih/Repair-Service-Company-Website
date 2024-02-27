5e05e384f6e9319aba90215e2911b0a67485fa2f
Alena <alyonatihomirova9@gmail.com>
2024-02-27 15:57:12 +0300

    Update

---
+++ README.md
@@ -0,0 +1,38 @@
+# Repair Services Website
+
+This is a website for Orbita Repair Services, a company offering repair services for computers and household appliances. The website provides information about the services offered, company background, client feedback, frequently asked questions, and contact details.
+
+## Features
+
+- **Services**: learn about the various repair services offered by Orbita Repair Services, including computer repair, appliance repair, electronic device repair, and more.
+
+- **About Us**: get to know about Orbita Repair Services and its dedication to providing top-notch repair services since the advent of computers.
+
+- **Client Feedback**: read testimonials from satisfied clients who have used Orbita Repair Services for their repair needs.
+
+- **Frequently Asked Questions**: find answers to common questions about repair services, warranty, device compatibility, and more.
+
+- **Contact Us**: reach out to Orbita Repair Services for reliable and prompt repair services. Contact information and a call-to-action button are provided for easy communication.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- Firebase Realtime Database
+- Font Awesome CDN
+
+## Database
+
+Customer contact information is stored in the Firebase Realtime Database.
+
+## Contributors
+
+- Alena Tikhomirova
+
+## Contribution
+
+If you have ideas to enhance this project or encounter any issues, feel free to contribute by opening an issue or submitting a pull request. Your input is highly valued!
+
+## Contact
+For inquiries or feedback, please contact me at alyonatihomirova9@gmail.com.
--- index.js
+++ index.js
@@ -19,9 +19,16 @@ const contactDatainDB = ref(database, "RepairServicesContactData")
 const mobileNav = document.getElementById("mobile-nav")
 const aboutYouMenu = document.getElementById("about-you-menu")
 
+const mobileNavRu = document.getElementById("mobile-nav-ru")
+const aboutYouMenuRu = document.getElementById("about-you-menu-ru")
+
 const inputNameEl = document.getElementById("input-name")
 const inputPhoneEl = document.getElementById("input-phone")
 const inputEmailEl = document.getElementById("input-email")
+
+const inputNameElRu = document.getElementById("input-name-ru")
+const inputPhoneElRu = document.getElementById("input-phone-ru")
+const inputEmailElRu = document.getElementById("input-email-ru")
 
 const feedbackContainer = document.getElementById("feedback-container")
 
@@ -43,6 +50,13 @@ document.addEventListener("click", function(e) {
         makeACall()
     } else if (e.target.id === "feedback-container" || feedbackContainer.contains(e.target)) {
         handleFeedbackClick()
+    } else if (e.target.id === "hamburger-ru") {
+        showMenuRu()
+    } else if (e.target.id === "about-you-item-ru") {
+        showYouMenuRu()
+    } else if (e.target.id === "submit-button-ru") {
+        e.preventDefault()
+        submitContactDataRu()
     }
 })
 
@@ -57,10 +71,27 @@ function showMenu() {
             <li><a href="#feedback">Feedback from our clients</a></li>
             <li><a href="#questions">FAQ</a></li>
             <li><a href="#contact">Contact</a></li>
+            <li><a href="localized-version.html">Ru</a></li>
         <ul>
     </div>`
     document.getElementById("mobile-menu-container").style.display = "flex"
     document.getElementById("mobile-menu-container").style.flexDirection = "column"
+}
+
+function showMenuRu() {
+    mobileNavRu.innerHTML = `
+    <div class="mobile-menu-container" id="mobile-menu-container-ru">
+        <ul>
+            <li><a href="#services">Услуги</a></li>
+            <li><a href="#about-us">О нас</a></li>
+            <li><a href="#feedback">Отзывы клиентов</a></li>
+            <li><a href="#questions">FAQ</a></li>
+            <li><a href="#contact">Контакты</a></li>
+            <li><a href="index.html">En</a></li>
+        <ul>
+    </div>`
+    document.getElementById("mobile-menu-container-ru").style.display = "flex"
+    document.getElementById("mobile-menu-container-ru").style.flexDirection = "column"
 }
 
 function showYouMenu() {
@@ -71,6 +102,16 @@ function showYouMenu() {
         </ul>`
 
     aboutYouMenu.style.display = "flex"
+}
+
+function showYouMenuRu() {
+    aboutYouMenuRu.innerHTML = `
+        <ul>
+            <li><a href="#feedback">Отзывы клиентов</a></li>
+            <li><a href="#questions">FAQ</a></li>
+        </ul>`
+
+    aboutYouMenuRu.style.display = "flex"
 }
 
 function rootClick(e) {
@@ -92,29 +133,125 @@ function rootClick(e) {
             !aboutYouMenu.contains(e.target)) {
             // Element clicked was not the dropdown aboutYouMenu — close the aboutYouMenu
             aboutYouMenu.style.display = "none"
-            }
+            } else if (mobileNavRu &&
+                        document.getElementById("mobile-menu-container-ru") &&
+                        document.getElementById("mobile-menu-container-ru").style.display === "flex" &&
+                        e.target.id !== "mobile-nav-ru" &&
+                        e.target.id !== "hamburger-ru" &&
+                        e.target.id !== "mobile-menu-container-ru" &&
+                        !mobileNavRu.contains(e.target)) {
+                            // Element clicked was not the mobile nav ru or its children — close the mobile nav ru
+                            document.getElementById("mobile-menu-container-ru").style.display = "none"
+                            mobileNavRu.innerHTML = `
+                                <i class="fa-solid fa-bars" id="hamburger-ru"></i>`
+                        } else if (aboutYouMenuRu &&
+                            aboutYouMenuRu.style.display === "flex" &&
+                            e.target.id !== "about-you-menu-ru" &&
+                            e.target.id !== "about-you-item-ru" &&
+                            !aboutYouMenuRu.contains(e.target)) {
+                            // Element clicked was not the dropdown aboutYouMenuRu — close the aboutYouMenuRu
+                            aboutYouMenuRu.style.display = "none"
+                            }
 }
 
-function submitContactData() {
-    const form = document.querySelector("form")
+// function submitContactData() {
+//     // const form = document.querySelector("form")
+//     const form = document.getElementById("form-en")
 
-    if (!form.checkValidity()) {
-        // If the form is not valid, prevent submition
-        // The form is valid when the name and email input fields are not empty
-        alert("Please type in your name and email ❤️")
-    } else {
-        alert(`Thank you, ${inputNameEl.value} ❤️ We will contact you soon!`)
-
+//     if (form) {
+//         if (!form.checkValidity()) {
+//             // If the form is not valid, prevent submition
+//             // The form is valid when the name and email input fields are not empty
+//             alert("Please type in your name and email ❤️")
+//         } else {
+//             alert(`Thank you, ${inputNameEl.value} ❤️ We will contact you soon!`)
     
-        // Update inputValue with input values
-        inputValue.name = inputNameEl.value
-        inputValue.phoneNumber = inputPhoneEl.value
-        inputValue.email = inputEmailEl.value
+        
+//             // Update inputValue with input values
+//             inputValue.name = inputNameEl.value
+//             inputValue.phoneNumber = inputPhoneEl.value
+//             inputValue.email = inputEmailEl.value
+    
+//             // Push the contact data item to the database
+//             push(contactDatainDB, inputValue)
+    
+//         }
+//     }
+// }
 
-        // Push the contact data item to the database
-        push(contactDatainDB, inputValue)
+function submitContactData() {
+    // const form = document.querySelector("form")
+    // const form = document.getElementById("form-en")
 
-    }
+        if (!inputNameEl.value) {
+            // If the form is not valid, prevent submition
+            // The form is valid when the name and email input fields are not empty
+            alert("Please type in your name and email ❤️")
+        } else if (!inputEmailEl.value) {
+            alert("Please type in your name and email ❤️")
+        } else {
+            alert(`Thank you, ${inputNameEl.value} ❤️ We will contact you soon!`)
+    
+        
+            // Update inputValue with input values
+            inputValue.name = inputNameEl.value
+            inputValue.phoneNumber = inputPhoneEl.value
+            inputValue.email = inputEmailEl.value
+    
+            // Push the contact data item to the database
+            push(contactDatainDB, inputValue)
+    
+        }
+}
+
+// function submitContactDataRu() {
+//     // const form = document.querySelector("form")
+//     const form = document.getElementById("form-ru")
+
+//     if (form) {
+//         if (!form.checkValidity()) {
+//             // If the form is not valid, prevent submition
+//             // The form is valid when the name and email input fields are not empty
+//             alert("Please type in your name and email ❤️")
+//         } else {
+//             alert(`Thank you, ${inputNameEl.value} ❤️ We will contact you soon!`)
+    
+        
+//             // Update inputValue with input values
+//             inputValue.name = inputNameElRu.value
+//             inputValue.phoneNumber = inputPhoneElRu.value
+//             inputValue.email = inputEmailElRu.value
+    
+//             // Push the contact data item to the database
+//             push(contactDatainDB, inputValue)
+    
+//         }
+//     }
+// }
+
+function submitContactDataRu() {
+    // const form = document.querySelector("form")
+    // const form = document.getElementById("form-ru")
+
+        if (!inputNameElRu.value) {
+            // If the form is not valid, prevent submition
+            // The form is valid when the name and email input fields are not empty
+            alert("Пожалуйста, укажите ваше имя и email ❤️")
+        } else if (!inputEmailElRu.value) {
+            alert("Пожалуйста, укажите ваше имя и email ❤️")
+        } else {
+            alert(`Спасибо, ${inputNameElRu.value} ❤️ Мы скоро свяжемся с вами!`)
+    
+        
+            // Update inputValue with input values
+            inputValue.name = inputNameElRu.value
+            inputValue.phoneNumber = inputPhoneElRu.value
+            inputValue.email = inputEmailElRu.value
+    
+            // Push the contact data item to the database
+            push(contactDatainDB, inputValue)
+    
+        }
 }
 
 function makeACall() {
